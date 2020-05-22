ae7976f593dc71ed5ad7efbd2dc78f31cf505f7f
anderskillander <55491817+anderskillander@users.noreply.github.com>
2020-05-22 10:14:53 +0200

    feat: added feels like & weather description

--- sketch.js
+++ sketch.js
@@ -4,9 +4,17 @@ let s = 0.0;
 
 let a2 = 0.0;
 let s2 = 0.0;
+let osc;
 
+let sound;
+let audioWave;
 let url =
   "http://api.openweathermap.org/data/2.5/weather?q=Stockholm&units=metric&appid=b48c7729bd6c5816e8c6fb60c20264a6";
+
+function preload() {
+  sound = loadSound("assets/rain.mp3");
+  audioWave = loadImage("assets/audio.png");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -16,6 +24,7 @@ function setup() {
   noFill();
   stroke(255, 204, 0);
   background(0);
+  button = createButton("Button");
 }
 
 function gotData(data) {
@@ -24,6 +33,7 @@ function gotData(data) {
 }
 
 function draw() {
+  image(audioWave, 200, 200, 20, 20);
   background("rgba(0,0,0,0.01)");
   a = a + 0.01;
   s = cos(a) * 3;
@@ -33,8 +43,11 @@ function draw() {
     var humidity = weather.main.humidity * 2;
 
     title = weather.name;
-    tempTitle = weather.main.temp + "C";
+    tempTitle = weather.main.temp + " °C";
+    feelsLike = "Feels like: " + weather.main.feels_like + " °C";
     humidityTitle = weather.main.humidity + "%";
+    descriptionTitle = weather.weather[0].main;
+    // console.log(typeof descriptionTitle);
 
     // Title
     push();
@@ -45,13 +58,26 @@ function draw() {
     // Temp Title
     push();
     textSize(32);
+    stroke(0, 204, 250);
     text(tempTitle, 400, 250);
+    pop();
+    // Feels Like Title
+    push();
+    textSize(32);
+    stroke(0, 204, 250);
+    text(feelsLike, 400, 300);
     pop();
     //Humidity Title
     textSize(32);
     push();
-    stroke(0, 204, 250);
-    text(humidityTitle, 400, 300);
+    stroke(255, 204, 0);
+    text(humidityTitle, 400, 350);
+    pop();
+    //Description Title
+    textSize(32);
+    push();
+    stroke(250, 100, 100);
+    text(descriptionTitle, 400, 400);
     pop();
 
     translate(width / 2, height / 2);
@@ -68,5 +94,21 @@ function draw() {
     stroke(255, 204, 0);
     ellipse(200, 0, humidity, humidity);
     pop();
+  }
+}
+
+function mousePressed() {
+  if (sound.isPlaying() == false) {
+    sound.play();
+  } else if (sound.isPlaying() == true) {
+    sound.stop();
+  }
+}
+
+function keyPressed() {
+  if (noLoop() == false) {
+    noLoop();
+  } else if (noLoop() == true) {
+    reset();
   }
 }
